8399a062e2aae9c2cf1d17e9ae6de0f275cd78d4
Joost Buskermolen <joostbuskermolen@hotmail.com>
2018-09-27 22:44:45 +0200

    Extract TCP segment

--- sniffer.py
+++ sniffer.py
@@ -48,4 +48,16 @@ def icmp_packet(data):
     icmp_type, code, checksum = struct.unpack('! B B H', data [:4])  # grab first 4 bytes (header)
     return icmp_type, code, checksum, data[4:]  # Add data, everything after 4th byte
 
+# Extract TCP segment based on the TCP IP packet diagram
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('! H H H L L H', data[:14]) # First 96bits from a TCP/IP packet
+    offset = (offset_reserved_flags >> 12) * 4  # Push Reserved & TCP Flags out by bit shifting to filter offset out
+    # Create all possible flag types
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, offset_reserved_flags, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
 main()
