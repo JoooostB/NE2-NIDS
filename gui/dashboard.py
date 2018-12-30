bdba8a14a43b35596d9feaece1d67a3211c3e672
Zandschuur <sander.terschure@gmail.com>
2018-12-30 16:39:37 +0100

    add bootstrap and css

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import subprocess
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,20 +7,5 @@ def test():
 
 @app.route('/')
 def dashboard():
-
-    def inner():
-        proc = subprocess.Popen(
-            test(),
-            shell=True,
-            stdout=subprocess.PIPE
-        )
-
-    while proc.poll() is None:
-        yield proc.stdout.readline() + '<br/>\n'
-    return flask.Response(inner(),
-                          mimetype='text/html')  # text/html is required for most browsers to show the partial page immediately
-
     user = {'username': 'Sander'}
-    return render_template('index.html', title='Dashboard sniffer', user=user)
-
-app.run(debug=True, port=5000)
+    return render_template('index.html', title='Home', user=user)
