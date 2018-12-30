958af5bcb53dd11a6ba351f726a5c3c08f41a422
Zandschuur <sander.terschure@gmail.com>
2018-12-30 15:45:05 +0100

    template flask setup

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+import subprocess
+
 app = Flask(__name__)
+
+def test():
+    print("TEst tsetsets")
 
 @app.route('/')
 def dashboard():
-    return 'hello world'
 
+    def inner():
+        proc = subprocess.Popen(
+            test(),
+            shell=True,
+            stdout=subprocess.PIPE
+        )
+
+    while proc.poll() is None:
+        yield proc.stdout.readline() + '<br/>\n'
+    return flask.Response(inner(),
+                          mimetype='text/html')  # text/html is required for most browsers to show the partial page immediately
+
+    user = {'username': 'Sander'}
+    return render_template('index.html', title='Dashboard sniffer', user=user)
+
+app.run(debug=True, port=5000)
