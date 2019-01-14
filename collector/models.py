1381f192a344d214a2cf8b0aca1be45947bac186
Zandschuur <sander.terschure@gmail.com>
2019-01-14 23:30:03 +0100

    Create docker containers for database, phpmyadmin and the collector

---
+++ collector/models.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+
+class Database:
+    def __init__(self):
+
+        self.con = mysql.connector.connect(user='ne2_admin',
+                                           password='appelflap',
+                                           host='localhost',
+                                           database='collector')
+        self.cur = self.con.cursor()
+
+    def list_packets(self):
+        self.cur.execute("SELECT protocol, src_address, bytes, packets, date FROM collector")
+        result = self.cur.fetchall()
+        self.con.close()
+
+        return result
+
--- gui/dashboard/dashboard.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, url_for
-import flask
-from sniffer.sniffer import sniffer
-
-app = Flask(__name__)
-
-@app.route('/')
-def dashboard():
-    return flask.Response(sniffer(), mimetype='text/html')
