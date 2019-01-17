7043374cf60418cf63d10ed3e5bc3a81c5ebf578
Zandschuur <sander.terschure@gmail.com>
2019-01-17 23:31:46 +0100

    New feature = Filter on packets in GUI

--- collector/collector.py
+++ collector/collector.py
@@ -1,26 +1,94 @@
+import datetime
+
 from flask import Flask, request, jsonify
 from flask import render_template
 from models import Database
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def hello():
+@app.route('/', methods=['GET', 'POST'])
+def status():
     if request.method == 'GET':
-        dbHandler = Database()
         print("get all packets from db")
 
         dbHandler = Database()
+        all_packets = dbHandler.list_packets()
+        print("all packets from db are = ", all_packets)
+
+        now = datetime.datetime.now().replace(microsecond=0)
+
+        filter_settings = request.form
+
+        print("Filter settings are = ", filter_settings)
+
+        return render_template('index.html', all_packets=all_packets, now=now.isoformat())
+
+    if request.method == 'POST':
+        filter_settings = request.form
+
+        if filter_settings['start_time']:
+            filter_start_time = filter_settings['start_time']
+            print("filter start_time = ", filter_start_time)
+        else:
+            print("filter start_time = empty")
+            filter_start_time = "*"
+
+        if filter_settings['end_time']:
+            filter_end_time = filter_settings['end_time']
+            print("filter end_time = ", filter_end_time)
+        else:
+            print("filter end_time = empty = ")
+            filter_end_time = datetime.datetime.utcnow().replace(microsecond=0)
+
+        filter_protocol = []
+        if 'protocol_tcp' in filter_settings:
+            filter_protocol.append("TCP")
+            print("filter_protocol = ", filter_protocol)
+        else:
+            print("filter_protocol_tcp is empty")
+
+        if 'protocol_udp' in filter_settings:
+            filter_protocol.append("UDP")
+            print("filter_protocol = ", filter_protocol)
+        else:
+            print("filter_protocol_udp is empty")
+
+        if 'protocol_icmp' in filter_settings:
+            filter_protocol.append("ICMP")
+            print("filter_protocol = ", filter_protocol)
+        else:
+            print("filter_protocol_icmp is empty")
+
+        if len(filter_protocol) != 0:
+            filter_protocol = '|'.join(filter_protocol)
+        else:
+            filter_protocol = "TCP|UDP|ICMP"
+        print("protocol is", filter_protocol)
+        print("start time is", filter_start_time)
+        print("end time is", filter_end_time)
+
+        dbHandler = Database()
+        filter_result = dbHandler.filter_db(filter_protocol, filter_start_time, filter_end_time)
+
+        print("print result = ", filter_result)
 
         all_packets = dbHandler.list_packets()
         print("all packets from db are = ", all_packets)
-        return render_template('index.html', all_packets=all_packets)
+
+        now = datetime.datetime.now().replace(microsecond=0)
+        print("now is het volgende ")
+
+        filter_settings = request.form
+
+        print("Filter settings are = ", filter_settings)
+
+        return render_template('index.html', all_packets=all_packets, now=now.isoformat(), filter_result=filter_result)
 
 
 @app.route('/insert_packet', methods=['POST'])
 def insert_db():
     if request.method == 'POST':
-
         packet = request.get_json(force=True)
 
         print("packet is = ", packet)
--- collector/models.py
+++ collector/models.py
@@ -21,7 +21,7 @@ class Database(object):
                 print(err)
 
     def list_packets(self):
-        self.cursor.execute("SELECT protocol, src_address, bytes, packets, date FROM collector")
+        self.cursor.execute("SELECT protocol, src_address, bytes, packets, date FROM collector ORDER BY `date` DESC")
         result = self.cursor.fetchall()
         return result
 
@@ -30,6 +30,14 @@ class Database(object):
         self.cursor.execute("INSERT INTO collector (protocol, src_address, bytes) VALUES (%s, %s, %s)",
                          (packet_protocol, packet_ip, packet_bytes))
         self.conn.commit()
+
+    def filter_db(self, filter_protocol, filter_start_time, filter_end_time):
+        print("applying filter with the following items", filter_protocol, filter_start_time, filter_end_time)
+        self.cursor.execute("SELECT protocol, src_address, bytes, packets, date FROM `collector` "
+                            "WHERE `protocol` REGEXP %s AND `date` BETWEEN %s AND %s ORDER BY `date` DESC",
+                            (filter_protocol, filter_start_time, filter_end_time,))
+        result = self.cursor.fetchall()
+        return result
 
     def __del__(self):
         self.conn.close()
