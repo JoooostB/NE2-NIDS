603fbb1e46e01bd9c741fa0d03551c82182baf43
Joost Buskermolen <joostbuskermolen@hotmail.com>
2019-01-17 10:22:01 +0100

    Extended Database to include __del__

--- collector/models.py
+++ collector/models.py
@@ -1,19 +1,33 @@
 import mysql.connector
+host = "db"
+user = "ne2-nids"
+passwd = "appelflap"
+database = "collector"
 
-
-class Database:
+class Database(object):
+    """This class is responsible for setting up a connection to the database and executing simple queries."""
     def __init__(self):
-
-        self.con = mysql.connector.connect(user='ne2_admin',
-                                           password='appelflap',
-                                           host='db',
-                                           database='collector')
-        self.cur = self.con.cursor()
+        try:
+            self.conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database)
+            if self.conn:
+                self.cursor = self.conn.cursor()
+                print('Success')
+        except mysql.connector.Error as err:
+            if err == 1049:
+                print('Unknown database: ' + database)
+            else:
+                print(err)
 
     def list_packets(self):
-        self.cur.execute("SELECT protocol, src_address, bytes, packets, date FROM collector")
+        self.cursor.execute("SELECT protocol, src_address, bytes, packets, date FROM collector")
         result = self.cur.fetchall()
-        self.con.close()
-
+        self.conn.close()
         return result
+
+    def clear(self):
+        return self.cursor.execute('TRUNCATE TABLE collector')
+
+    def __del__(self):  # When object goes out of scope, close connection.
+        self.conn.commit()
+        self.conn.close()
 
